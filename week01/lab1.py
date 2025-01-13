983dc41c516d62ea15e46ae54aee16fe3e9e27cd
Ivan-here <ivan.baranash@gmail.com>
2025-01-13 17:29:43 -0500

    added first lab

--- week01/lab1.py
+++ week01/lab1.py
@@ -4,4 +4,12 @@ arr = [1, 4, 7, 9]
 a=1
 b=2
 c=3
+d=4
 
+e = (a - b) ** (c // d) + (a % c)
+
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Please enter your age: ")) + 22
+print("Now showing the shop items filtered by age: " + str(userAge))
