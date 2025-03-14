9be4455e0fa8c857d2eaef18ae3a8add4a5a278f
Ivan-here <ivan.baranash@gmail.com>
2025-03-14 15:56:48 -0400

    ..

--- week02/lab02exercise.py
+++ week02/lab02exercise.py
@@ -1,26 +1,44 @@
-import random
-choices = ["Rock", "Paper", "Scissors"]
+'''
+Author: Ivan Barnash
+Assignment: #1
+'''
 
-def main():
-    try:
-        user_choice = input("Enter your choice: ").capitalize()
-        if user_choice not in choices:
-            raise ValueError("Invalid choice, enter Rock, Paper, Scissors")
-        player_choice = choices.index(user_choice)
-        computer_choice = random.randint(0,2)
-        print("your choice is:", choices[player_choice])
-        print("computer's choice is:", choices[computer_choice])
+gym_member = "Alex Alliton" # str
+preferred_weight_kg = 20.5  # float
+highest_reps = 25           # int
+membership_active = True    # bool
 
-        if player_choice == computer_choice:
-            print("Draw")
-        elif ((player_choice == 0 and computer_choice == 2) or
-              (player_choice == 1 and computer_choice == 0) or
-              (player_choice == 2 and computer_choice == 1)):
-            print("You win")
-        else:
-            print("Computer wins")
-    except ValueError as e: print(f"Error: {e}")
+#creating dictionary
+workout_stats = {
+    "Alex": (30, 45, 20),
+    "Jamie": (25, 50, 30),
+    "Taylor": (40, 35, 25)
+}
 
+total_minutes = {person + "_Total": sum(minutes) for person, minutes in workout_stats.items()}
+workout_stats.update(total_minutes)
 
-if __name__ == "__main__":
-    main()
+workout_list = [list(minutes) for person, minutes in workout_stats.items() if "_Total" not in person]
+
+print("\nYoga and running minutes for all people:")
+print([row[:2] for row in workout_list])
+print("\nWeightlifting minutes for the last two people:")
+print([row[2] for row in workout_list[-2:]])
+
+for person, total in workout_stats.items():
+    if "_Total" in person and total >= 120:
+        print(f"Great job staying active, {person.replace('_Total', '')}!") #removing _Total
+
+person_name = input("Enter a name: ")
+if person_name in workout_stats:
+    minutes = workout_stats[person_name]
+    print(f"Workout minutes for {person_name}: Yoga: {minutes[0]}, Running: {minutes[1]}, Weightlifting: {minutes[2]}")
+    print(f"Total workout minutes: {workout_stats[f'{person_name}_Total']}")
+else:
+    print(f"Person {person_name} not found in the records.")
+
+person_totals = {person: total for person, total in workout_stats.items() if "_Total" in person}
+highest = max(person_totals, key=person_totals.get).replace("_Total", "")
+lowest = min(person_totals, key=person_totals.get).replace("_Total", "")
+print(f"\nA person with highest workout time: {highest}")
+print(f"\nA person with lowest workout time: {lowest}")
